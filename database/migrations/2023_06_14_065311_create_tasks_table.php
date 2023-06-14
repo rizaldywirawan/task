ca5687c8d5fb11efc628a348a2d28792cc824bcd
Rizaldy Wirawan <rizaldy.wirawan@gmail.com>
2023-06-14 17:25:14 +0700

    feat: backend api - authentication using sanctum

---
+++ database/migrations/2023_06_14_065311_create_tasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->text('title');
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('due_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+            $table->enum('status', ['To-do', 'On Progress', 'In Review', 'Done', 'Canceled'])->nullable();
+            $table->enum('priority', ['#1', '#2', '#3', '#4'])->nullable();
+            $table->foreignUuid('project_id')->nullable()->references('id')->on('projects');
+            $table->foreignUuid('created_by')->references('id')->on('users');
+            $table->foreignUuid('updated_by')->nullable()->references('id')->on('users');
+            $table->foreignUuid('deleted_by')->nullable()->references('id')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
