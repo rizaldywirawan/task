6e146ea4694eed901b21262ecdbd2010c1505251
Rizaldy Wirawan <rizaldy.wirawan@gmail.com>
2023-06-15 19:56:59 +0700

    fix: task relationship naming to assignees

--- app/Actions/Task/AssignUserToTask.php
+++ app/Actions/Task/AssignUserToTask.php
@@ -9,8 +9,7 @@ class AssignUserToTask
 {
     public function handle(Task $task, User $user): Task
     {
-        $task->taskAssignees()->attach($user->id);
-
-        return $task->load('taskAssignees');
+        $task->assignees()->attach($user->id);
+        return $task->load('assignees');
     }
 }
