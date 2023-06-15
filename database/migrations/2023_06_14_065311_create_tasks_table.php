3a64a2504077120ad28e225638b596a93f39819c
Rizaldy Wirawan <rizaldy.wirawan@gmail.com>
2023-06-15 11:28:29 +0700

    fix: add default value for the status

--- database/migrations/2023_06_14_065311_create_tasks_table.php
+++ database/migrations/2023_06_14_065311_create_tasks_table.php
@@ -17,8 +17,9 @@ return new class extends Migration
             $table->timestamp('started_at')->nullable();
             $table->timestamp('due_at')->nullable();
             $table->timestamp('completed_at')->nullable();
-            $table->enum('status', ['To-do', 'On Progress', 'In Review', 'Done', 'Canceled'])->nullable()->default('To-do');
+            $table->enum('status', ['To-do', 'On Progress', 'In Review', 'Done', 'Canceled'])->default('To-do');
             $table->enum('priority', ['#1', '#2', '#3', '#4'])->nullable();
+            $table->integer('order')->nullable();
             $table->foreignUuid('project_id')->nullable()->references('id')->on('projects');
             $table->foreignUuid('created_by')->references('id')->on('users');
             $table->foreignUuid('updated_by')->nullable()->references('id')->on('users');
